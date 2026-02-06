d0a7685c2e57ca60abaa94a85f929a17249f0998
DESKTOP-5UL4J7C\BMD Tech <amadou527@gmail.com>
2026-02-06 12:28:00 +0100

    Fix Hotels.jsx import and add data.js

---
+++ src/data.js
@@ -0,0 +1,26 @@
+// src/data.js
+const hotels = [
+  {
+    id: 1,
+    name: "Hotel One",
+    location: "Dakar",
+    price: 50000,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "Hotel Two",
+    location: "Thiès",
+    price: 45000,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 3,
+    name: "Hotel Three",
+    location: "Mbour",
+    price: 60000,
+    image: "https://via.placeholder.com/150"
+  }
+];
+
+export default hotels;
